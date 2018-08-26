602e4055a5e329b0e146536fee83136943aa5bb1
Matthew Larner <matt.larner.dev@gmail.com>
2018-08-26 14:28:02 +1000

    bar

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,26 @@ module.exports = {
     plugins: [
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-styled-components',
-        'gatsby-plugin-netlify-cms',
         {
             resolve: `gatsby-plugin-google-fonts`,
             options: {
                 fonts: [`Unica One`],
             },
         },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: 'pages',
+            },
+        },
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [],
+            },
+        },
+        `gatsby-transformer-remark`,
+        `gatsby-plugin-netlify-cms`,
     ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 
-const IndexPage = () => <div />;
+const IndexPage = ({ data }) => {
+    console.log(data);
+    return <div>Hi</div>;
+};
+
+IndexPage.propTypes = {
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            edges: PropTypes.array,
+        }),
+    }),
+};
 
 export default IndexPage;
+
+// export const pageQuery = graphql`
+//   query IndexQuery {
+//     allMarkdownRemark(
+//       sort: { order: DESC, fields: [frontmatter___date] },
+//       filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+//     ) {
+//       edges {
+//         node {
+//           excerpt(pruneLength: 400)
+//           id
+//           fields {
+//             slug
+//           }
+//           frontmatter {
+//             title
+//             templateKey
+//             date(formatString: "MMMM DD, YYYY")
+//           }
+//         }
+//       }
+//     }
+//   }
